ccd45a68468f7b3cfe740f91f818c85e60661b0f
Aparna2817 <tenaliaparna2817@gmail.com>
2024-06-07 19:41:42 +0530

    Add files via upload

---
+++ data_storage.py
@@ -0,0 +1,11 @@
+def add_expense(amount, description, category):
+    expense = {
+        'amount': amount,
+        'description': description,
+        'category': category
+    }
+    expenses.append(expense)
+    print("Expense added successfully!")
+
+def get_expenses():
+    return expenses
